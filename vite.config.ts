31f495aa0e4c5eed2662e93a750b0ad96954214b
Penguin <say8425@gmail.com>
2023-03-08 20:10:51 +0900

    📦 rollup alias 제거
    * vite 4 에서 기본 지원이라서 필요 없음

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,8 @@
-import { dirname, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
-
-import alias from "@rollup/plugin-alias";
 import { defineConfig } from "vite";
 import eslint from "vite-plugin-eslint";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const projectRootDirectory = resolve(__dirname);
-export default defineConfig(() => ({
-  plugins: [
-    eslint(),
-    alias({
-      entries: [
-        {
-          find: "@",
-          replacement: resolve(projectRootDirectory, "src"),
-        },
-      ],
-    }),
-  ],
+export default defineConfig({
+  plugins: [eslint()],
   build: {
     target: "esnext",
     emptyOutDir: true,
@@ -35,4 +19,4 @@ export default defineConfig(() => ({
       },
     },
   },
-}));
+});
